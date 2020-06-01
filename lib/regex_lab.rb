74296b08da21b8b361cedffc1fd5c276aa0ac42d
Olivia Cubela <oliviacubela@gmail.com>
2020-06-01 17:35:01 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,5 +1,9 @@
 def starts_with_a_vowel?(word)
-  word =~ /\b+[aeiouAEIOU]/
+  word = "apple"
+if word.match(/\b+[aeiouAEIOU]/) 
+  print "true"
+else 
+  print "false"
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
